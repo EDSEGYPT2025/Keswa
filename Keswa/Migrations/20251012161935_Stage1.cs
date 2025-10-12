ff59e57ad06c169cd2b9827e3b841aafd73078e0
Ahmed samir <edsegypt@outlook.com>
2025-10-12 19:36:02 +0300

    Stage11

---
+++ Keswa/Migrations/20251012161935_Stage1.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Keswa.Migrations
+{
+    /// <inheritdoc />
+    public partial class Stage1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Colors_ColorId",
+                table: "Products");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Products_ColorId",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "ColorId",
+                table: "Products");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ColorId",
+                table: "Products",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_ColorId",
+                table: "Products",
+                column: "ColorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Colors_ColorId",
+                table: "Products",
+                column: "ColorId",
+                principalTable: "Colors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
