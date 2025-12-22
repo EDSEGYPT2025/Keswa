05288ce61c9b4c4a6c16aa32b1b4f2aca73a2518
Ahmed samir <edsegypt@outlook.com>
2025-12-22 20:00:25 +0200

    Stage

---
+++ Keswa/Migrations/20251106202415_StageJu.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Keswa.Migrations
+{
+    /// <inheritdoc />
+    public partial class StageJu : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RemainingQuantity",
+                table: "FinishingAssignments",
+                newName: "TotalScrapped");
+
+            migrationBuilder.RenameColumn(
+                name: "AssignmentType",
+                table: "FinishingAssignments",
+                newName: "ReceivedQuantity");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TotalScrapped",
+                table: "FinishingAssignments",
+                newName: "RemainingQuantity");
+
+            migrationBuilder.RenameColumn(
+                name: "ReceivedQuantity",
+                table: "FinishingAssignments",
+                newName: "AssignmentType");
+        }
+    }
+}
